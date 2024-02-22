1c493d6970b734875932c241bb3520ba421389d3
Danielle <daniellem@spacelift.io>
2024-02-22 10:14:10 +0000

    last test

--- multimodule-main 2/pet/main.tf
+++ multimodule-main 2/pet/main.tf
@@ -17,12 +17,6 @@ resource "random_pet" "this" {
   }
 }
 
-resource "random_pet" "that" {
-  keepers = {
-    version = var.version_number
-  }
-}
-
 variable "example_variable" {
   default = null
 }
