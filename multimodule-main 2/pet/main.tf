1692551bd715a1baa7ccf8be50c5ae262c660583
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-10-13 14:33:14 +0100

    Update main.tf

--- multimodule-main 2/pet/main.tf
+++ multimodule-main 2/pet/main.tf
@@ -1,25 +1,4 @@
-variable "version_number" {
-  description = "Version number required by the pet module"
-  type        = number
-  # You can specify a default value or leave it without a default to make it required.
-}
-
 module "pet" {
   source = "../"
 }
 
-resource "random_pet" "this" {
-  keepers = {
-    version = var.version_number
-  }
-}
-
-resource "random_pet" "that" {
-  keepers = {
-    version = var.version_number
-  }
-}
-
-variable "example_variable7" {
-  default = null
-}
